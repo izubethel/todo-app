a59107b7c0d961ef1932c9e13470443f29d7428a
izubethel <michaelokpalaoguchi@gmail.com>
2022-04-25 15:36:08 -0400

    todo

---
+++ todo.js
@@ -0,0 +1,7 @@
+//Create a Todo List App that should do/have the following
+//Have an input text field and a “ add” button
+//The todo typed in the input field should be appended to an unordered list in the DOM on Enter key press and add button click.
+//Each todo on the DOM should have a button or icon or anchor tag with “Done” label. On click of the Done button / icon / link; the todo should be removed from the DOM.
+//Implement the Todo using an array
+
+
